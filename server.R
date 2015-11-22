2c214572991eee1466e401f33ba78522a8cb2727
Sara Rafi <SaraRafi@users.noreply.github.com>
2015-11-23 04:21:17 +0500

    Update server.R
    fixed a bug.

--- server.R
+++ server.R
@@ -4,24 +4,24 @@ library(datasets)
 
 data(iris)
 
-##install.packages("caret", dependencies=TRUE)
-library(caret)
+#install.packages("caret", dependencies=TRUE)
+#library(caret)
 library( randomForest )
 
 ###########Sampling data
-set.seed(224)
-trainIndex <- createDataPartition(iris$Species, p=0.80, list=FALSE)
-data_train <- iris[ trainIndex,]
-data_test <- iris[-trainIndex,]
+#set.seed(224)
+#trainIndex <- createDataPartition(iris$Species, p=0.80, list=FALSE)
+#data_train <- iris[ trainIndex,]
+#data_test <- iris[-trainIndex,]
 
 ##########
-m <-train( Species ~ ., data=data_train, method="rf" )
+m <-randomForest( Species ~ ., data=iris )
 
 
 ##########################################
 
 
-library(shiny)
+library(shinyapps)
 
 
 shinyServer(
@@ -29,7 +29,7 @@ shinyServer(
    
     output$Result <- renderPrint({
                   y<-cbind(input$slider1,input$slider2,input$slider3,input$slider4)
-        colnames(y)<-names(data_train[1:4])
+        colnames(y)<-names(iris[1:4])
                   z<-predict(m, newdata=y)
             paste("Iris Specie:",z)
       
